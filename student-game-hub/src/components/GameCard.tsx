cb0571dbbd4ce3d5368b4a7c91db9807856a17d5
Javier Valencia <javier.valencia@app4u.com>
2023-05-15 08:26:03 -0400

    Displaying Platform Icons

--- student-game-hub/src/components/GameCard.tsx
+++ student-game-hub/src/components/GameCard.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Image, Heading } from "@chakra-ui/react";
+import { Card, CardBody, Image, Heading, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
 }
 
-function GameCard({ game }: Props) {
+const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
-}
+};
 
 export default GameCard;
---
+++ student-game-hub/src/components/PlatformIconList.tsx
@@ -0,0 +1,45 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { IconType } from "react-icons";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { Text, Icon, HStack } from "@chakra-ui/react";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  // using index signature on the iconMap object
+  const iconMap: { [key: string]: IconType } = {
+    // name: PlayStation
+    // slug: playstation
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    Android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;

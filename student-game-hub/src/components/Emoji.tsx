0044ab786c1e0ef5f311afe5951116244016d1cb
Javier Valencia <javier.valencia@app4u.com>
2023-05-27 09:23:53 -0400

    Add emojis

---
+++ student-game-hub/src/components/Emoji.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import bullEye from "../assets/bulls-eye.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+import meh from "../assets/meh.webp";
+import { Image, ImageProps } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+  // index signature
+  // using this syntax with can tell the typescript compiler
+  // that this object can have any number of keys
+  // and the keys are numbers
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullEye, alt: "exceptional", boxSize: "35px" },
+  };
+
+  return <Image {...emojiMap[rating]} marginTop={1} />;
+};
+
+export default Emoji;
